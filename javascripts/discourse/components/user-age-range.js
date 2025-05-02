e427a192c702dad4133e588bd3c2c49dd72a29bc
fethi bey abi ayad <fethi.bey.abi.ayad.ccsmtl@ssss.gouv.qc.ca>
2025-05-02 14:46:47 -0400

    test 28

---
+++ javascripts/discourse/components/user-age-range.js
@@ -0,0 +1,32 @@
+import Component from "@glimmer/component";
+import { hbs } from "ember-cli-htmlbars";
+
+export default class UserAgeRangeComponent extends Component {
+  get ageRangeLabel() {
+    const rawAge = this.args.user?.user_fields?.[2]; // Remplace 2 si nécessaire
+    if (!rawAge) return null;
+
+    const age = parseInt(rawAge, 10);
+    if (isNaN(age)) {
+      if (rawAge === "26+") {
+        return "26 ans et plus";
+      } else {
+        return "Âge non défini";
+      }
+    }
+
+    if (age >= 14 && age <= 17) return "14 - 17 ans";
+    if (age >= 18 && age <= 25) return "18 - 25 ans";
+    if (age >= 26) return "26 ans et plus";
+
+    return "Âge non défini";
+  }
+
+  static template = hbs`
+    {{#if this.ageRangeLabel}}
+      <div class="user-age-range">
+        Tranche d'âge : {{this.ageRangeLabel}}
+      </div>
+    {{/if}}
+  `;
+}
